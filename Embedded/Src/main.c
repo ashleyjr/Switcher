1a9db680646ca1ea3e159e092d8646a24693345a
Ashley Robinson <ashley181291@gmail.com>
2016-12-03 13:20:50 +0000

    two pwm outputs in phase

--- Embedded/Src/main.c
+++ Embedded/Src/main.c
@@ -5,44 +5,49 @@
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
-// Includes
+// Defines
 //-----------------------------------------------------------------------------
-
-#include "SI_C8051F850_Register_Enums.h"
-#include "SI_C8051F850_Defs.h"
-#include "InitDevice.h"
-#include "Pid.h"
-#include "Uart.h"
-
-//-----------------------------------------------------------------------------
-// Global CONSTANTS
-//-----------------------------------------------------------------------------
-#define DEBUG			0						// Set to 1 to remove debug output
+#define DEBUG			1						// Set to 1 to remove debug output
 #define SYSCLK      	24500000   				// SYSCLK frequency in Hz
 #ifdef DEBUG
 	#define BAUDRATE   	115200    				// Baud rate of UART in bps
 #endif
 
 //-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+#include "SI_C8051F850_Register_Enums.h"
+#include "SI_C8051F850_Defs.h"
+#include "InitDevice.h"
+#include "Pid.h"
+#include "Uart.h"
+#ifdef DEBUG
+	#include "stdio.h"
+#endif
+
+//-----------------------------------------------------------------------------
 // Global Variables
 //-----------------------------------------------------------------------------
-U8 	pwm;       									// Holds current PCA compare value
+U8 	pwm_0;       								// Holds current PCA compare value
+U8 	pwm_1;       								// Holds current PCA compare value
 
 //-----------------------------------------------------------------------------
 // MAIN Routine
 //-----------------------------------------------------------------------------
 
 void main (void){
+	pwm_0 = 0;
+	pwm_1 = 0;
 	initDevice();
 	pidInit();
-	pwm = 0;
 	while (1){
-		pwm = ADC0 >> 2;
-		pwm = pidUpdate(pwm);
-		uartSend(uartGet());
+		pwm_0 = ADC0 >> 2;
+		pwm_1 = pwm_0;
+		//pwm = pidUpdate(pwm);
+		//uartSend(uartGet());
 		#if DEBUG
 			SCON0_TI = 1;                 		// This STDIO library requres TI to be set for prints to occur
-			printf("%d,%hu mV\n",ADC0,pwm);
+			printf("%hu,%hu\n",pwm_0,pwm_1);
 		#endif
 	}
 }
