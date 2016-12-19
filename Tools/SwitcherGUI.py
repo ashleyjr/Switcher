81e2e553d554990b68c9101f7c7c4edafbdb4ebe
Ashley Robinson <ashley181291@gmail.com>
2016-12-19 23:19:37 +0000

    plotting many lines and using args

--- Tools/SwitcherGUI.py
+++ Tools/SwitcherGUI.py
@@ -1,87 +1,13 @@
 import random
 import serial
+import argparse
 import wx
-import matplotlib
-matplotlib.use('WXAgg')
-from matplotlib.figure import Figure
+from matplotlib.figure import Figure    # matplotlib.use('WXAgg')
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigCanvas
 import pylab
 import numpy as np
 
-
-class DataGen(object):
-    """ A silly class that generates pseudo-random data for
-        display in the plot.
-    """
-
-    def __init__(self, init=50):
-        self.data = self.init = init
-
-    def next(self):
-        self.update_rand()
-        return self.data
-
-    def update_rand(self):
-        delta = random.uniform(-0.5, 0.5)
-        r = random.random()
-
-        if r > 0.9:
-            self.data += delta * 15
-        elif r > 0.8:
-            # attraction to the initial value
-            delta += (0.5 if self.init > self.data else -0.5)
-            self.data += delta
-        else:
-            self.data += delta
-
-
-class BoundControlBox(wx.Panel):
-    """ A static box with a couple of radio buttons and a text
-        box. Allows to switch between an automatic mode and a
-        manual mode with an associated value.
-    """
-
-    def __init__(self, parent, ID, label, initval):
-        wx.Panel.__init__(self, parent, ID)
-
-        self.value = initval
-
-        box = wx.StaticBox(self, -1, label)
-        sizer = wx.StaticBoxSizer(box, wx.VERTICAL)
-
-        self.radio_auto = wx.RadioButton(self, -1,
-                                         label="Auto", style=wx.RB_GROUP)
-        self.radio_manual = wx.RadioButton(self, -1,
-                                           label="Manual")
-        self.manual_text = wx.TextCtrl(self, -1,
-                                       size=(35, -1),
-                                       value=str(initval),
-                                       style=wx.TE_PROCESS_ENTER)
-
-        self.Bind(wx.EVT_UPDATE_UI, self.on_update_manual_text, self.manual_text)
-        self.Bind(wx.EVT_TEXT_ENTER, self.on_text_enter, self.manual_text)
-
-        manual_box = wx.BoxSizer(wx.HORIZONTAL)
-        manual_box.Add(self.radio_manual, flag=wx.ALIGN_CENTER_VERTICAL)
-        manual_box.Add(self.manual_text, flag=wx.ALIGN_CENTER_VERTICAL)
-
-        sizer.Add(self.radio_auto, 0, wx.ALL, 10)
-        sizer.Add(manual_box, 0, wx.ALL, 10)
-
-        self.SetSizer(sizer)
-        sizer.Fit(self)
-
-    def on_update_manual_text(self, event):
-        self.manual_text.Enable(self.radio_manual.GetValue())
-
-    def on_text_enter(self, event):
-        self.value = self.manual_text.GetValue()
-
-    def is_auto(self):
-        return self.radio_auto.GetValue()
-
-    def manual_value(self):
-        return self.value
+tick_ms = 100
 
 
 class GraphFrame(wx.Frame):
@@ -89,24 +15,33 @@ class GraphFrame(wx.Frame):
     """
     title = 'Switcher'
 
-    def __init__(self):
+    def __init__(self,args):
         wx.Frame.__init__(self, None, -1, self.title)
 
-        self.ser = serial.Serial("COM100", 9600)
-
-        self.datagen = DataGen()
-        self.data = [self.datagen.next()]
-
+        self.adc1_data = [0]
+        self.adc2_data = [0]
+        self.adc3_data = [0]
+        self.plot_adc1_data = None
+        self.plot_adc2_data = None
+        self.plot_adc3_data = None
         self.adc1 = False
         self.adc2 = False
         self.adc3 = False
-        self.current = False
+        self.fig = None
+        self.axis = None
+        self.panel = None
+        self.canvas = None
+        self.cb_adc1 = None
+        self.cb_adc2 = None
+        self.cb_adc3 = None
+        self.hbox1 = None
+        self.vbox = None
 
+        self.ser = serial.Serial(args.com_port, args.baud_rate)
         self.create_main_panel()
-
         self.redraw_timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.on_redraw_timer, self.redraw_timer)
-        self.redraw_timer.Start(100)
+        self.redraw_timer.Start(tick_ms)
 
     def create_main_panel(self):
         self.panel = wx.Panel(self)
@@ -114,24 +49,19 @@ class GraphFrame(wx.Frame):
         self.init_plot()
         self.canvas = FigCanvas(self.panel, -1, self.fig)
 
-        self.xmin_control = BoundControlBox(self.panel, -1, "X min", 0)
-        self.xmax_control = BoundControlBox(self.panel, -1, "X max", 50)
-
-        self.cb_adc1= wx.CheckBox(self.panel, -1,"Read ADC1",style=wx.ALIGN_RIGHT)
+        self.cb_adc1 = wx.CheckBox(self.panel, -1, "Read input voltage", style=wx.ALIGN_LEFT)
         self.cb_adc1.SetValue(False)
 
-        self.cb_adc2 = wx.CheckBox(self.panel, -1, "Read ADC2", style=wx.ALIGN_RIGHT)
+        self.cb_adc2 = wx.CheckBox(self.panel, -1, "Read top side of current shunt", style=wx.ALIGN_LEFT)
         self.cb_adc2.SetValue(False)
 
-        self.cb_adc3 = wx.CheckBox(self.panel, -1, "Read ADC3", style=wx.ALIGN_RIGHT)
+        self.cb_adc3 = wx.CheckBox(self.panel, -1, "Read output voltage", style=wx.ALIGN_LEFT)
         self.cb_adc3.SetValue(False)
 
         self.hbox1 = wx.BoxSizer(wx.VERTICAL)
         self.hbox1.Add(self.cb_adc1, border=5, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
         self.hbox1.Add(self.cb_adc2, border=5, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
         self.hbox1.Add(self.cb_adc3, border=5, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
-        self.hbox1.Add(self.xmin_control, border=5, flag=wx.ALL)
-        self.hbox1.Add(self.xmax_control, border=5, flag=wx.ALL)
 
         self.vbox = wx.BoxSizer(wx.HORIZONTAL)
         self.vbox.Add(self.canvas, 1, flag=wx.LEFT | wx.TOP | wx.GROW)
@@ -141,50 +71,26 @@ class GraphFrame(wx.Frame):
         self.vbox.Fit(self)
 
     def init_plot(self):
-        self.dpi = 100
-        self.fig = Figure((3.0, 3.0), dpi=self.dpi)
+        self.fig = Figure((5.0, 5.0), 100)
 
         self.axes = self.fig.add_subplot(111)
-        self.axes.set_axis_bgcolor('black')
+        self.axes.set_axis_bgcolor('white')
 
         pylab.setp(self.axes.get_xticklabels(), fontsize=8)
         pylab.setp(self.axes.get_yticklabels(), fontsize=8)
 
-        # plot the data as a line series, and save the reference
-        # to the plotted line series
-        #
-        self.plot_data = self.axes.plot(
-            self.data,
-            linewidth=1,
-            color=(1, 1, 0),
-        )[0]
+        self.plot_adc1_data = self.axes.plot(self.adc1_data, color=(1, 0, 0), linewidth=1, )[0]
+        self.plot_adc2_data = self.axes.plot(self.adc2_data, color=(0, 1, 0), linewidth=1, )[0]
+        self.plot_adc3_data = self.axes.plot(self.adc3_data, color=(0, 0, 1), linewidth=1, )[0]
 
     def draw_plot(self):
-        """ Redraws the plot
-        """
-        # when xmin is on auto, it "follows" xmax to produce a
-        # sliding window effect. therefore, xmin is assigned after
-        # xmax.
-        #
-        if self.xmax_control.is_auto():
-            xmax = len(self.data) if len(self.data) > 50 else 50
-        else:
-            xmax = int(self.xmax_control.manual_value())
 
-        if self.xmin_control.is_auto():
-            xmin = xmax - 50
-        else:
-            xmin = int(self.xmin_control.manual_value())
-
-        # for ymin and ymax, find the minimal and maximal values
-        # in the data set and add a mininal margin.
-        #
-        # note that it's easy to change this scheme to the
-        # minimal/maximal value in the current display, and not
-        # the whole data set.
-        #
-        ymin = round(min(self.data), 0) - 1
-        ymax = round(max(self.data), 0) + 1
+        top = float(len(self.adc1_data)*tick_ms) / 1000
+        time_s = np.linspace(0, top, len(self.adc1_data))
+        xmin = 0
+        xmax = time_s[len(time_s)-1]
+        ymin = round(np.amin(np.concatenate([self.adc1_data, self.adc2_data, self.adc3_data])), 0) - 1
+        ymax = round(np.amax(np.concatenate([self.adc1_data, self.adc2_data, self.adc3_data])), 0) + 1
         self.axes.set_xbound(lower=xmin, upper=xmax)
         self.axes.set_ybound(lower=ymin, upper=ymax)
 
@@ -195,32 +101,60 @@ class GraphFrame(wx.Frame):
         #
         self.axes.grid(True, color='gray')
 
-        self.plot_data.set_xdata(np.arange(len(self.data)))
-        self.plot_data.set_ydata(np.array(self.data))
+        self.plot_adc1_data.set_xdata(time_s)
+        self.plot_adc2_data.set_xdata(time_s)
+        self.plot_adc3_data.set_xdata(time_s)
+
+        self.plot_adc1_data.set_ydata(np.array(self.adc1_data))
+        self.plot_adc2_data.set_ydata(np.array(self.adc2_data))
+        self.plot_adc3_data.set_ydata(np.array(self.adc3_data))
 
         self.canvas.draw()
 
     def on_redraw_timer(self, event):
-        # if paused do not add data, but still redraw the plot
-        # (to respond to scale modifications, grid change, etc.)
-        #
+
+        """ ADC1 plotting"""
         if self.cb_adc1.IsChecked():
             self.ser.write("ADC1 \n\r")
+            self.adc1_data.append(random.random())
+        else:
+            self.adc1_data.append(self.adc1_data[len(self.adc1_data)-1])
+
+        """ ADC2 plotting"""
         if self.cb_adc2.IsChecked():
             self.ser.write("ADC2 \n\r")
+            self.adc2_data.append(random.random())
+        else:
+            self.adc2_data.append(self.adc2_data[len(self.adc2_data)-1])
+
+        """ ADC3 plotting"""
         if self.cb_adc3.IsChecked():
             self.ser.write("ADC3 \n\r")
+            self.adc3_data.append(random.random())
+        else:
+            self.adc3_data.append(self.adc3_data[len(self.adc3_data)-1])
 
-        self.data.append(self.datagen.next())
         self.draw_plot()
 
     def on_exit(self):
         self.Destroy()
 
 
+def get_args():
+    """Get command line arguments as a dictionary."""
+    parser = argparse.ArgumentParser(description="Switcher serial")
+    parser.add_argument('--port', dest='com_port', required=True, help='Serial port connected to switcher')
+    parser.add_argument('--baud', dest='baud_rate', required=True, help='Baud rate for switcher UART')
+    return parser.parse_args()
 
-if __name__ == '__main__':
-    app = wx.PySimpleApp()
-    app.frame = GraphFrame()
+
+def main():
+    args = get_args()
+    app = wx.App(False)
+    app.frame = GraphFrame(args)
     app.frame.Show()
     app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
