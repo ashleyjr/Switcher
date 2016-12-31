91b08e706a72625e8d1c2f1f1708ecb8e07e7556
Ashley Robinson <ashley181291@gmail.com>
2016-12-31 17:11:21 +0000

    tool to graph code size over commitsa

--- Tools/CodeSize.py
+++ Tools/CodeSize.py
@@ -1,16 +1,43 @@
 import subprocess
-
+import os
+import matplotlib.pyplot as plt
 
 def main():
+    os.chdir("..")
+    print subprocess.check_output("cd", shell=True)
     stdout = subprocess.check_output("git rev-list master", shell=True)
     commits = stdout.split("\n")
-    #for commit in commits:
-    #    if(len(commit) > 10):
-    #        cmd = "git checkout -f " + str(commit)
-    #        print cmd
-    #        stdout = subprocess.check_output(cmd, shell=True)
-    cmd = "git checkout -f " + str(commits[0])
-    subprocess.check_output(cmd, shell=True)                                    # Back top head
+    size = []
+    try:
+        for commit in commits:
+            if(len(commit) > 10):
+                cmd = "git checkout -f " + str(commit) + " Embedded\\Objects\\Switcher.build_log.htm"
+                print cmd
+                stdout = subprocess.check_output(cmd, shell=True)
+            f = open('Embedded\\Objects\\Switcher.build_log.htm', 'r+')
+            text = f.read()
+            start = text.index("code=")
+            start += 5
+            found = ""
+            while(True):
+                if(text[start] == "\n"):
+                    break
+                else:
+                    found += text[start]
+                    start += 1
+            size = [int(found)] + size
+            f.close()
+    except:
+        print "error"
+    print size
+    line_x = [0,len(size)]
+    line_y = [1024,1024]
+    plt.plot(line_x,line_y)
+    plt.plot(size)
+    plt.ylabel('Code size (bytes)')
+    plt.xlabel('Commit')
+    plt.grid(True)
+    plt.show()
 
 if __name__ == '__main__':
     main()
